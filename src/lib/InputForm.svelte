10acd3f3dca6d689295893d1e2438876ab5eb065
Varinder Singh <varinder.singh01@nagarro.com>
2023-05-28 14:27:04 +0530

    v1.0.0

---
+++ src/lib/InputForm.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import SelectDirectoryModal from "./SelectDirectoryModal.svelte";
+
+  let input = "";
+  let inputType = "file";
+  let isSelectDirectoryModalOpen = false;
+
+  const toggleSelectDirectoryModalOpen = () => {
+    isSelectDirectoryModalOpen = !isSelectDirectoryModalOpen;
+  };
+
+  const handleInputChange = (e) => {
+    console.log(e.target.value);
+    input = e.target.value.trim();
+  };
+
+  const handleDropdownValueChange = (e) => {
+    console.log(e.target.value);
+    inputType = e.target.value;
+  };
+</script>
+
+<div>
+  <div>
+    <input id="input" type="text" value={input} on:change={handleInputChange} />
+  </div>
+  <div>
+    <select
+      name="input_type"
+      id="input_type"
+      on:change={handleDropdownValueChange}
+      value={inputType}
+    >
+      <option value="file">File</option>
+      <option value="folder">Folder</option>
+    </select>
+  </div>
+  <div>
+    <button on:click={toggleSelectDirectoryModalOpen}>Save</button>
+    <button>Cancel</button>
+  </div>
+  {#if isSelectDirectoryModalOpen && input}
+    <SelectDirectoryModal on:modalClose={toggleSelectDirectoryModalOpen} />
+  {/if}
+</div>
---
+++ src/lib/SelectDirectoryModal.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<div id="modal" class="modal">
+  <h2>
+    Select Directory <button on:click={() => dispatch("modalClose")}
+      >Close</button
+    >
+  </h2>
+
+  <button>Save</button>
+  <button on:click={() => dispatch("modalClose")}>Cancel</button>
+</div>
+
+<style>
+  .modal {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 10;
+  }
+</style>
